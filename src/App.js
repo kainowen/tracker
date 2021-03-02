219de83cffdbd206a303d6a965b86efeca3161ee
TQALiam <liam@tquila-automation.com>
2021-03-02 17:01:31 +0000

    Create App, Adds bootstrap and React router dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App"/>
+      <Route path="/1"> <h1> Hello </h1> </Route>
+      <Route path="/2"> <h1> Goodbye </h1> </Route>
+    </BrowserRouter>
   );
 }
 
