95a720f10def670b5165fe0f5c058688318a7c72
TQALiam <liam@tquila-automation.com>
2021-03-03 14:37:49 +0000

    Adds Firebase to base project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import firebase from 'firebase/firebase';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App"/>
+      <div className="App" />
       <Route path="/1"> <h1> Hello </h1> </Route>
       <Route path="/2"> <h1> Goodbye </h1> </Route>
     </BrowserRouter>
---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+apiKey: "api-key",
+authDomain: "project-id.firebaseapp.com",
+databaseURL: "https://project-id.firebaseio.com",
+projectId: "project-id",
+storageBucket: "project-id.appspot.com",
+messagingSenderId: "sender-id",
+appId: "app-id",
+measurementId: "G-measurement-id",
+};
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
